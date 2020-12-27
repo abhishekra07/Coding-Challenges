3ad82922a6d58f8e0affe8d63c4c43644efaebee
abhishekra07 <40576705+abhishekra07@users.noreply.github.com>
2020-12-27 19:50:24 +0530

    Update PossibleSumToK.py

--- PossibleSumToK.py
+++ PossibleSumToK.py
@@ -5,3 +5,14 @@ For Example, given [10, 15, 3 ,7] and k of 17, retunr true since 10 + 7 is 17.
 
 try to do it in one pass
 """
+# use twon pointers one start from begining another from end
+def approach1(numbers, k):
+    j = len(numbers) - 1
+    for i in range(0,len(numbers)):
+        if i < j and (numbers[i] + numbers[j] == k):
+            return True
+        
+    return False
+
+  
+print(approach1([10, 15, 3 ,7], 17))
