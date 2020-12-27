fbd5528386a1c67baef18269f3029e51fca4e74c
abhishekra07 <40576705+abhishekra07@users.noreply.github.com>
2020-12-27 19:45:45 +0530

    Create PossibleSumToK.py

---
+++ PossibleSumToK.py
@@ -0,0 +1,7 @@
+"""
+Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+
+For Example, given [10, 15, 3 ,7] and k of 17, retunr true since 10 + 7 is 17.
+
+try to do it in one pass
+"""
